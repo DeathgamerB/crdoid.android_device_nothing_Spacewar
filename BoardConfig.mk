45474c445c6197280e5e7708165f6caeb26ba7c4
DeathgamerB <87858321+DeathgamerB@users.noreply.github.com>
2023-12-30 12:14:40 +0100

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -216,3 +216,6 @@ CONFIG_IEEE80211AX := true
 
 # Include the proprietary files BoardConfig.
 include vendor/nothing/Spacewar/BoardConfigVendor.mk
+
+# Others
+BUILD_BROKEN_GENRULE_SANDBOXING := false
